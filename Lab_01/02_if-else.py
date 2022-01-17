710f019b6a33e370a7fe43b66b526d844270e25d
Tuan Vuong <vptuan@users.noreply.github.com>
2022-01-17 13:57:44 +0000

    Add files via upload

---
+++ Lab_01/02_if-else.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  6 16:48:04 2022
+
+@author: Tuan
+
+"""
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    if (n % 2 == 1): 
+        print("Weird")
