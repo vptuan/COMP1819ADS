fc5e4f88449c2ae502c518724779921b26a03593
Tuan Vuong <vptuan@users.noreply.github.com>
2022-01-18 10:17:54 +0000

    Update 05_read from file.py

--- Lab_01/05_read from file.py
+++ Lab_01/05_read from file.py
@@ -6,6 +6,8 @@ Created on Tue Jan 14 01:36:23 2020
 @author: tuan
 """
 
-f = open("lucky_ids.txt", "r")
-for bannerId in f:
-  print(bannerId, end="")
+f = open("lucky_ids_4.txt", "r")
+with open("lucky_ids_4.txt", "r") as f:
+    ids = [int(x) for x in next(f).split()] # read first line
+print(ids)
+# YOUR CODE HERE
