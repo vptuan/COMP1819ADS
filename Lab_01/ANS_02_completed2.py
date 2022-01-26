54e7abf357b122df10c8eee67103eae66b9ee470
Tuan Vuong <vptuan@users.noreply.github.com>
2022-01-26 15:44:49 +0000

    Add files via upload

---
+++ Lab_01/ANS_02_completed2.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  6 16:48:04 2022
+
+@author: Tuan
+
+"""
+#!/bin/python3
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    if (n % 2 == 1) or (10<= n <= 30): 
+        print("Weird")
+    else :
+        print("Not Weird")
+8
