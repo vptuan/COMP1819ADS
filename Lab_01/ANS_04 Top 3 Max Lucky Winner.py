10bbc6cf3217a82b6f777dc740033de39e237f1d
Tuan Vuong <vptuan@users.noreply.github.com>
2022-01-26 16:07:11 +0000

    Add files via upload

---
+++ Lab_01/ANS_04 Top 3 Max Lucky Winner.py
@@ -0,0 +1,19 @@
+with open("lucky_ids_4.txt", "r") as f:
+    stuff = []
+    for i in f.readline().split():
+        stuff.append(int(i))
+    greatest1 = stuff[0]
+    greatest2 = stuff[0]
+    greatest3 = stuff[0]
+    for i in stuff:
+        if i > greatest1:
+            greatest3 = greatest2
+            greatest2 = greatest1
+            greatest1 = i
+        elif greatest1 > i > greatest2:
+            greatest3 = greatest2
+            greatest2 = i
+        elif greatest2 > i > greatest3:
+            greatest3 = i
+        print(i, greatest1, greatest2, greatest3)
+    print(greatest1, greatest2, greatest3)
