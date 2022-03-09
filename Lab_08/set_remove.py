f41b92946123041de9744cf1657a5ce4d497b785
Tuan Vuong <vptuan@users.noreply.github.com>
2022-03-09 09:43:20 +0000

    Add files via upload

---
+++ Lab_08/set_remove.py
@@ -0,0 +1,28 @@
+# Adapted from https://www.geeksforgeeks.org/python-sets/
+# Python program to demonstrate
+# Deletion of elements in a Set
+
+# Creating a Set
+set1 = set([1, 2, 3, 4, 5, 6,
+			7, 8, 9, 10, 11, 12])
+print("Initial Set: ")
+print(set1)
+
+# Removing elements from Set
+# using Remove() method
+set1.remove(5)
+print("\nSet after Removal of two elements: ")
+print(set1)
+
+# Removing elements from Set
+# using Discard() method
+set1.discard(8)
+print("\nSet after Discarding two elements: ")
+print(set1)
+
+# Removing elements from Set
+# using iterator method
+for i in range(1, 5):
+	set1.remove(i)
+print("\nSet after Removing a range of elements: ")
+print(set1)
