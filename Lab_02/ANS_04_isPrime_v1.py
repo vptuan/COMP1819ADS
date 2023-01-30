1fe39303a010e797297058d1d1ba5e829bfecf0c
Tuan Vuong <vptuan@users.noreply.github.com>
2023-01-30 19:28:24 +0000

    Update ANS_04_isPrime_v1.py

--- Lab_02/ANS_04_isPrime_v1.py
+++ Lab_02/ANS_04_isPrime_v1.py
@@ -14,9 +14,15 @@ n = 111181177 # 8 secs
 
 start = time.time()
 
+# factors is a list of integers from 1 to n (inclusive), such that n is divisible by f.
+# The expression f for f in range(1, n+1) creates a sequence of numbers from 1 to n (inclusive) and assigns each number to the variable f. 
+# The if clause if n % f == 0 specifies that only the elements of the sequence for which the condition n % f == 0 is true should be included in the resulting list. 
+# The % operator represents modulo, so n % f == 0 is true if and only if n is divisible by f.
 factors = [f for f in range (1,n+1) if n%f==0 ]
+
 print(factors)
 
+# is_prime when a numbers having exactly 2 factors
 is_prime = len(factors) == 2
 
 print(is_prime)
