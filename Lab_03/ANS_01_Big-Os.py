e5577deb2572b640c330a37a3c61c1fe7b18b453
Tuan Vuong <vptuan@users.noreply.github.com>
2023-01-30 17:37:29 +0000

    Update ANS_01_Big-Os.py

--- Lab_03/ANS_01_Big-Os.py
+++ Lab_03/ANS_01_Big-Os.py
@@ -1,12 +1,18 @@
-# Sample solution from a student in COMP1819-2022
 import time
 
+'''
+The time complexity of the above code is O(1) (constant time). 
+The loop iterates a fixed number of times (10000), regardless of the value of the input variable n. 
+The operations performed inside the loop also have a constant time complexity. 
+The time taken to execute this code will always be the same and does not depend on the size of the input variable n.
+'''
 def constant(n):
     x = 0
+    count = 0
     for x in range(10000):
         x = x + len(n)
-
-
+        count += 1
+    return 
 
 def logarithmic(n):
     x = len(n)
@@ -14,14 +20,21 @@ def logarithmic(n):
     while x >1:
         x = x / 2
         count = count + 1
+    return count
 
-
-
+'''
+The time complexity of the function 'linear(n)' is O(n) 
+because the number of operations being performed is directly proportional to the size of input n. 
+The function is iterating through the input list 'n' and 
+performing an operation on each element, so the number of operations is directly proportional to the number of elements in the list.
+'''
 def linear(n):
     x = 0
+    count = 0
     for i in n:
         x = x + i
-
+        count += 1
+    return count
 
 def linear_logarithmic(n):
     x = len(n)
@@ -31,45 +44,67 @@ def linear_logarithmic(n):
         while x > 1:
             x = x / 2
             count = count + 1
+    return count
 
-
-def square(n):
+'''
+The time complexity of the above code is O(n^2), also known as quadratic. 
+The inner loop iterates through the entire input size "n" for each iteration of the outer loop, 
+thus resulting in a total of n*n operations, or n^2 operations.
+'''
+def quadratic(n):
+    count = 0
+    x = 0
     for i in n:
-        n[i] = i * 2
-        for x in n:
-            n[i] = i * 2
-
+        for j in n:
+            x += i * j
+            count += 1
+    return count
 
 def cube(n):
+    count = 0
     for i in n:
-        n[i] = i * 2
         for x in n:
-            n[i] = i * 2
             for y in n:
-                n[i] = i * 2
-
-
-def timetaken(size):
+                count += 1
+    return count
+    
+def runCase(size):
+    #creates a list sequence of integers from 0 to size-1 using a for loop
     sequence = []
     for i in range(size):
         sequence.append(i)
+        
+    #starts a timer
     starttime = time.time()
-    ######################## select Big-0 here ########################
+    
+    #calls one of the functions depending on which one is enabled. 
     #constant(sequence)
     #logarithmic(sequence)
     #linear(sequence)
+    #quadratic(sequence)
     #linear_logarithmic(sequence)
-    #square(sequence)
-    #cube(sequence)
-    ######################## select Big-0 here ########################
+    cube(sequence)
+    
+    #calculates the total time taken by subtracting the start time from the current time. 
     totaltime = time.time() - starttime
+    
     return (totaltime)
 
+'''
+The function runCases takes in a list of case sizes.
+For each size in the list, it calls the runCase function passing the size as an argument. 
+The runCase function creates a sequence of integers of the given size, gets the current time 
+and calls the function whose time complexity is being analyzed (constant, linear, or quadratic). 
+The function then calculates the total time taken by subtracting the current time from the start time. 
+The function then prints out the size, the time taken in standard form, and the time taken in decimal form. 
+The function runCases is used to report the time complexity of the different functions (constant, linear, or quadratic) with different sizes.
+'''
+def runCases(caseSizes):
+    print("Size  Timetaken(Standard Form)  Timetaken (Decimal)")
+    for n in caseSizes:
+        tk = runCase(n)
+        print(str(n)+"  "+ str(tk)+ " " + format(tk, ".2f"))
 
-def graphing(testnumbers):
-    print("__________GRAPHING__________")
-    for i in testnumbers:
-        print(str(i)+"  "+str(timetaken(i)))
+caseSizes = [10,100,300,900]
 
-numbersizes = [1024,1000,10000,100000,10000000,20000000,100000000,200000000]
-graphing(numbersizes)
+runCases(caseSizes)
