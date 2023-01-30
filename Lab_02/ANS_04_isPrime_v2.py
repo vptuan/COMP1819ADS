cbfe24b0d720db165e349a6b5e4c04aeb43af9dd
Tuan Vuong <vptuan@users.noreply.github.com>
2023-01-30 19:30:15 +0000

    Update ANS_04_isPrime_v2.py

--- Lab_02/ANS_04_isPrime_v2.py
+++ Lab_02/ANS_04_isPrime_v2.py
@@ -13,8 +13,15 @@ n = 111181177 # 10 secs
 
 start = time.time()
 
+"""
+The given code checks whether a given number n is a prime number or not. 
+The is_prime variable is initialized as True. 
+The code uses a for loop that iterates over a range of numbers from 2 to n-1. 
+Inside the loop, the code checks if n is divisible by i (the current iteration number) by checking if n % i == 0. 
+If it is divisible, it means i is a factor of n and n is not a prime number, so is_prime is set to False and the loop is broken. 
+If the loop completes without finding a factor, is_prime remains True and n is considered a prime number.
+"""
 is_prime = True
-
 for i in range(2, n):
     if n % i == 0:
         is_prime = False
