b6e78e4fc66c7f6237c0c84898abfa9877b73aef
Tuan Vuong <vptuan@users.noreply.github.com>
2023-01-30 19:34:05 +0000

    Update ANS_04_isPrime_v3.py

--- Lab_02/ANS_04_isPrime_v3.py
+++ Lab_02/ANS_04_isPrime_v3.py
@@ -6,6 +6,13 @@ n = 111181177 # 4 secs
 
 start = time.time()
 
+"""
+The code checks whether a given number n is a prime number or not. 
+If n is greater than 1, the code enters a for loop which runs from 2 to int(n/2). 
+Within the loop, it checks if n is divisible by i using the modulo operator %. 
+If the result is 0, it means that n is divisible by i and hence not prime, so is_prime is set to False and a message is printed with the factor i. 
+If the result is not 0, it means that n is not divisible by i and the loop continues.
+"""
 is_prime = True
 
 if n <= 1:
@@ -16,8 +23,6 @@ else:
             is_prime = False
             print("factor ", i)
             break
-        else:
-            is_prime = True
 
 print(is_prime)
 
