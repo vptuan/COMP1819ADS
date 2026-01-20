32751d6b3b25980440ca514b9c4974e2fbd19cdf
Tuan Vuong <vptuan@users.noreply.github.com>
2026-01-20 00:17:14 +0000

    Update ANS_G1.py

--- Lab_01/ANS_G1.py
+++ Lab_01/ANS_G1.py
@@ -1,16 +1,29 @@
 import random
+import time
+
+def minmax(sequence):
+    min = max = sequence[0] # assuming no-empty
+    for val in sequence:
+        #time.sleep(0.01)
+        if (val > max):
+            max = val
+        if (val < min):
+            min = val
+    return (min,max)
 
 def generate_numbers():
     print("--- COMP 1819 Data Generation ---")
     print("Generating 40 random numbers between 1 and 1,000,000...\n")
     
     # Generate 40 unique random numbers
-    dataset = random.sample(range(1, 1000001), 40)
+    dataset = random.sample(range(1, 1000001), 400)
     
     print(dataset)
+    print("(Min, Max) = ", minmax(dataset))
 
     print("\n---------------------------------")
     print("Copy the numbers above for your submission.")
 
+    
 if __name__ == "__main__":
     generate_numbers()
